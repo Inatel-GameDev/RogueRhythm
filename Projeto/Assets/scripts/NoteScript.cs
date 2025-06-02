b7befd62c1dd351ff05980281ffaf8c6df793382
talecozinho <talecozinho@gmail.com>
2025-06-02 20:11:45 -0300

    Checkpoint.

--- Projeto/Assets/scripts/NoteScript.cs
+++ Projeto/Assets/scripts/NoteScript.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class NoteScript : MonoBehaviour
 {
+    [SerializeField]
+    float noteSpeed = 2f;
+
+    Vector2 direction = Vector2.left;
+
+    Rigidbody2D body;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+
+        body.AddForce(direction * noteSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
