8ec8ef95ee32f329d461e3988bf165f4694dae95
talecozinho <talecozinho@gmail.com>
2025-06-02 19:38:24 -0300

    O Mergre prometido!

---
+++ Projeto/Assets/scripts/GameLogic.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    bool[,] Songs = { { true, false, true, true }, { false, true, true, false }, { true, true, true, false }, { true, false, false, false } };
+
+    int size;
+
+    [SerializeField]
+    Transform[] Spawners;
+    [SerializeField]
+    GameObject Note;
+
+    float timeBetweenBeats = 1.0f;
+
+    void Start()
+    {
+        StartCoroutine(PlayMusic()); 
+    }
+
+/*
+void Update()
+{
+
+}
+*/
+
+IEnumerator PlayMusic()
+    {
+        size = Songs.GetLength(0);
+        while (true)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (Songs[i, j] == true)
+                    {
+                        Instantiate(Note, Spawners[j]);
+                    }
+                }
+                yield return new WaitForSeconds(timeBetweenBeats);
+            }
+        }
+
+    }
+
+}
